8656267c204ae72deb1b5566317ac3133ca1122b
JoAn <joannacalinska@gmail.com>
2018-12-12 22:37:21 +0100

    day2-ex2

--- app/Node.js_challenge_dzien_2/zadanie02.js
+++ app/Node.js_challenge_dzien_2/zadanie02.js
@@ -1,1 +1,21 @@
-//Twój kod
+// Napisz program, który wyświetli zawartość wszystkich plików w folderze `app/data/zadanie02/`.
+
+// Skorzystaj z możliwości listowania plików w folderze. Po otrzymaniu listy plików wyświetlaj je w pętli.
+
+const fs = require('fs');
+
+fs.readdir('./data/zadanie02', (error, data) => {
+  if (error === null) {
+    data.forEach(file => {
+      fs.readFile(`./data/zadanie02/${file}`, 'utf8', (error, data) => {
+        if (error === null) {
+          console.log(`${file}: ${data}`);
+        } else {
+          console.log(error);
+        }
+      })
+    })
+  } else {
+    console.log(error);
+  }
+})
--- app/Node.js_challenge_dzien_2/zadanieDnia.js
+++ app/Node.js_challenge_dzien_2/zadanieDnia.js
@@ -1,1 +1,4 @@
-//Twój kod
+// Dzisiejsze zadanie dnia pozwoli Ci stworzyć program "TrAwKa". Ma on modyfikować plik tekstowy w taki sposób, aby każdy nieparzysty znak był pisany wielką literą, a parzysty małą literą.
+
+// Efekt będzie nieco przypominał nieprzystrzyżony trawnik ;) Np. plik z zawartością `Hello, World!` zamieni na `HeLlO, wOrLd!`.
+
