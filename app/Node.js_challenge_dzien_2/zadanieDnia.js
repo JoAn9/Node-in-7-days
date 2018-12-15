dcdbd81df5ed7149e2937b97c18e1eb521ed96e9
JoAn <joannacalinska@gmail.com>
2018-12-16 00:08:23 +0100

    day2-ex.3

--- app/Node.js_challenge_dzien_2/zadanieDnia.js
+++ app/Node.js_challenge_dzien_2/zadanieDnia.js
@@ -2,3 +2,19 @@
 
 // Efekt będzie nieco przypominał nieprzystrzyżony trawnik ;) Np. plik z zawartością `Hello, World!` zamieni na `HeLlO, wOrLd!`.
 
+const fs = require('fs');
+
+fs.readFile('./data/zadanieDnia/test.txt', 'utf8', (error, data) => {
+  if (error === null) {
+    let text = [];
+    for(let i=0; i<data.length; i++) {
+      if (i % 2 === 0) text[i] = data[i].toUpperCase()
+      else text[i] = data[i].toLowerCase();
+    }
+    text = text.join('');
+    fs.writeFile('./data/zadanieDnia/trawka.txt', text, (error) => {
+      if (error === null) console.log('Saved! :)')
+      else console.log('Not saved because: ' + error)
+    });
+  }
+})
